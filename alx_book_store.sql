50e462ad58d5318a1151f67a96204af230ed148c
isri21 <israelalemu21@gmail.com>
2024-10-20 18:43:36 +0300

    Modified Task 0 #1

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 use alx_book_store;
-CREATE TABLE Books(
+CREATE TABLE IF NOT EXISTS Books(
 	book_id INT PRIMARY KEY,
 	title VARCHAR(130),
 	author_id INT,
@@ -10,19 +10,19 @@ CREATE TABLE Books(
 	FOREIGN KEY (author_id) REFERENCES Auhtors(author_id)
 );
 
-CREATE TABLE Authors(
+CREATE TABLE IF NOT EXISTS Authors(
 	author_id INT PRIMARY KEY,
 	author_name VARCHAR(215)
 );
 
-CREATE TABLE Customers(
+CREATE TABLE IF NOT EXISTS Customers(
 	customer_id INT PRIMARY KEY,
 	customer_name VARCHAR(215),
 	email VARCHAR(215),
 	address TEXT
 );
 
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
 	order_id INT PRIMARY KEY,
 	customer_id INT,
 	order_date DATE,
@@ -30,13 +30,13 @@ CREATE TABLE Orders(
 	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
 	order_detail INT PRIMARY KEY,
 	order_id INT,
 	book_id INT,
 
 	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-	FOREIGN KEY (book_id) REFERENCES Books(book_id_id)
+	FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
 
